93ccef8a64814b10d67db47edf85de81b2ec531a
barakamulungula <mulungulab@gmail.com>
2018-04-16 14:17:10 -0400

    AddedVideoGameDatabase and VideoGameApplication class

---
+++ app/src/main/java/com/example/barakamulungula/videogamelibrary/VideoGameDatabase.java
@@ -0,0 +1,15 @@
+package com.example.barakamulungula.videogamelibrary;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+
+@Database(version = 1, entities = VideoGame.class)
+@TypeConverters(DateConverter.class)
+
+
+public abstract class VideoGameDatabase extends RoomDatabase{
+
+    public abstract VideoGameDao videoGameDao();
+
+}
